27c9b3bff1c7d90a28006613f0d28d53503aa12f
micnncim <micnncim@gmail.com>
2020-05-07 05:26:25 +0900

    Add inputs.labels

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,54 +3,44 @@
 set -e
 
 prefix=${INPUT_LABEL_PREFIX}
-github_token=${INPUT_GITHUB_TOKEN}
+labels=${INPUT_LABELS}
 
-run() {
-    pull_request=''
+case "${GITHUB_EVENT_NAME}" in
 
-    case "${GITHUB_EVENT_NAME}" in
-    'push')
-        pull_request="$(fetch_merged_pull_request)"
-        ;;
-    'pull_request')
-        pull_request="$(jq '.pull_request' "${GITHUB_EVENT_PATH}")"
-        ;;
-    *)
-        echo "::debug:: event ${GITHUB_EVENT_NAME} not supported"
-        ;;
-    esac
+'push')
+    for l in ${labels}; do
+        case ${l#"$prefix"} in # e.g.) 'release/major' => 'major'
+        'major' | 'minor' | 'patch')
+            if [ -z "${label}" ]; then
+                label="${l}"
+            else
+                label="${label}\n${l}"
+            fi
+            ;;
+        esac
+    done
+    ;;
 
-    if [ -z "${pull_request}" ]; then
-        echo "::error:: failed to get pull request event"
-    fi
+'pull_request')
+    label=$(jq -r ".pull_request.labels[].name | select(test(\"$prefix(major|minor|patch)\"))" "${GITHUB_EVENT_PATH}")
+    ;;
 
-    label=$(echo "${pull_request}" | jq -r ".labels[].name | select(test(\"$prefix(major|premajor|minor|preminor|patch|prepatch|prerelease)\"))")
+*)
+    echo "::debug:: event ${GITHUB_EVENT_NAME} not supported"
+    ;;
 
-    if [ -z "${label}" ]; then
-        echo "::debug:: no release label"
-        exit 0
-    fi
+esac
 
-    if [ "$(echo "${label}" | wc -l)" -ne 1 ]; then
-        echo "::error:: multiple release labels not allowed: labels=$(echo "${label}" | tr '\n' ',')"
-        exit 1
-    fi
+if [ -z "${label}" ]; then
+    echo "::debug:: no release label"
+    exit 0
+fi
 
-    level=${label#"${prefix}"} # e.g.) 'release/major' => 'major'
+if [ "$(echo "${label}" | wc -l)" -ne 1 ]; then
+    echo "::error:: multiple release labels not allowed: labels=$(echo "${label}" | tr '\n' ' ')"
+    exit 1
+fi
 
-    echo "::set-output name=level::${level}"
-}
+level=${label#"${prefix}"} # e.g.) 'release/major' => 'major'
 
-fetch_merged_pull_request() {
-    if [ -z "${github_token}" ]; then
-        echo "::error:: github token is required for push event"
-        exit 1
-    fi
-
-    curl -s \
-        -H "Authorization: token ${github_token}" \
-        "https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls?state=closed&sort=updated&direction=desc" |
-        jq -r ".[] | select(.merge_commit_sha==\"${GITHUB_SHA}\")"
-}
-
-run
+echo "::set-output name=level::${level}"
